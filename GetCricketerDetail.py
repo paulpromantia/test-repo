57e20eb6a05927723c8f9d80b9be3c3c34f73b37
Paul Clinton <paul.clinton.partner@decathlon.com>
2022-05-26 15:20:20 +0530

    Revert "Merge pull request #3 from paulpromantia/11091_git_demo"
    This reverts commit 4771f972a6b8b63ca5c323e0f0ef02a1ec5a5b07, reversing
    changes made to c06f14d2ae1206ddbadedac6ba190b9a24bb1551.

--- GetCricketerDetail.py
+++ GetCricketerDetail.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 excel_file_path="cricketer_data.xlsx"
 df = pd.read_excel(excel_file_path, 'Raw Data')
-print("Welcome!!")
+
 cricketer_name = input("Enter Cricket Name: ")
 
 cricketer_details=df.loc[df['Name'] == cricketer_name ]
 
 if not cricketer_details.empty:
-    print(cricketer_detail)
+    print(cricketer_details)
 else:
     print("Details not found. Please try another name.")
